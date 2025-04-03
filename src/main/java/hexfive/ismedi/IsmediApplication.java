d99f0e9eb07adc3e429d40349abfb8ac07c4dfa7
성유진 <syj@syj.local>
2025-04-03 13:08:27 +0900

    chore: Spring Boot project setup

---
+++ src/main/java/hexfive/ismedi/IsmediApplication.java
@@ -0,0 +1,13 @@
+package hexfive.ismedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsmediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsmediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hexfive/ismedi/IsmediApplicationTests.java
@@ -0,0 +1,13 @@
+package hexfive.ismedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsmediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
