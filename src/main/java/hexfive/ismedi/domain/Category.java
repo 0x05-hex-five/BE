a5cd6b691da4439269c0b5b7485df2db401e23d1
성유진 <syj@syj.local>
2025-04-04 14:52:51 +0900

    chore: category length 제거

--- src/main/java/hexfive/ismedi/domain/Category.java
+++ src/main/java/hexfive/ismedi/domain/Category.java
@@ -1,20 +1,32 @@
 package hexfive.ismedi.domain;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.*;
 
 @Entity
+@Getter
+@NoArgsConstructor
 @Table(name = "Category")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false)
     private String displayName;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false)
     private String selectName;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Medicine> medicines;
+
+    @Builder
+    public Category(String displayName, String selectName) {
+        this.displayName = displayName;
+        this.selectName = selectName;
+    }
 }
