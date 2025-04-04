d6d57573a6febaea6ff7b5110c2345c275f0c119
jin20fd <syj010820@naver.com>
2025-04-04 15:42:43 +0900

    refact: 응답 형식 설정, 성공 응답인 경우 적용

--- src/main/java/hexfive/ismedi/category/CategoryController.java
+++ src/main/java/hexfive/ismedi/category/CategoryController.java
@@ -3,6 +3,7 @@ package hexfive.ismedi.category;
 import hexfive.ismedi.category.dto.CreateCategoryDto;
 import hexfive.ismedi.category.dto.ResCategoryDto;
 import hexfive.ismedi.category.dto.UpdateCategoryDto;
+import hexfive.ismedi.global.APIResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,27 +16,28 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @PostMapping
-    public ResCategoryDto createCategory(@RequestBody CreateCategoryDto createCategoryDto) {
-        return categoryService.createCategory(createCategoryDto);
+    public APIResponse<ResCategoryDto> createCategory(@RequestBody CreateCategoryDto createCategoryDto) {
+        return APIResponse.success(categoryService.createCategory(createCategoryDto));
     }
 
     @GetMapping("/{id}")
-    public ResCategoryDto getCategory(@PathVariable Long id) {
-        return categoryService.getCategory(id);
+    public APIResponse<ResCategoryDto> getCategory(@PathVariable Long id) {
+        return APIResponse.success(categoryService.getCategory(id));
     }
 
     @GetMapping
-    public List<ResCategoryDto> getAllCategories() {
-        return categoryService.getAllCategories();
+    public APIResponse<List<ResCategoryDto>> getAllCategories() {
+        return APIResponse.success(categoryService.getAllCategories());
     }
 
     @PutMapping("/{id}")
-    public ResCategoryDto updateCategory(@PathVariable Long id, @RequestBody UpdateCategoryDto updateCategoryDto) {
-        return categoryService.updateCategory(id, updateCategoryDto);
+    public APIResponse<ResCategoryDto> updateCategory(@PathVariable Long id, @RequestBody UpdateCategoryDto updateCategoryDto) {
+        return APIResponse.success(categoryService.updateCategory(id, updateCategoryDto));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCategory(@PathVariable Long id) {
+    public APIResponse<Void> deleteCategory(@PathVariable Long id) {
         categoryService.deleteCategory(id);
+        return APIResponse.success(null);
     }
 }
