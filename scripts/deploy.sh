965a64f08b4520769f1309e37c142b14d23e35a1
jin20fd <syj010820@naver.com>
2025-04-15 15:53:23 +0900

    fix: deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,21 +5,19 @@ set -e
 APP_DIR=~/ismedi
 cd $APP_DIR
 
-echo "[1] docker 설치 확인"
 if ! command -v docker &> /dev/null; then
     sudo yum update -y
-    sudo amazon-linux-extras enable docker
     sudo yum install docker -y
     sudo systemctl start docker
     sudo systemctl enable docker
 fi
+echo "[1] docker 설치 완료"
 
-echo "[2] docker-compose 설치 확인"
 if ! command -v docker-compose &> /dev/null; then
-  echo "Docker Compose 설치 중"
   sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
+echo "[2] docker-compose 설치 완료"
 
 echo "[3] 컨테이너 재시작"
 DB_ROOT_PASSWORD=$1 DB_USER_PASSWORD=$2 docker compose pull
