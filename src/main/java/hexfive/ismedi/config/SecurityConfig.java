86f9cbdcfb9eb72e4009f66eae0fb5c20a735c5a
jin20fd <syj010820@naver.com>
2025-04-22 22:17:10 +0900

    chore: SecurityConfig 폴더 위치 변경

--- src/main/java/hexfive/ismedi/config/SecurityConfig.java
+++ src/main/java/hexfive/ismedi/config/SecurityConfig.java
@@ -1,5 +1,7 @@
-package hexfive.ismedi.jwt;
+package hexfive.ismedi.config;
 
+import hexfive.ismedi.jwt.JwtAuthenticationFilter;
+import hexfive.ismedi.jwt.JwtProvider;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
