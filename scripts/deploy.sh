bf19b6adbfb4bca0106f0a41d9c8eed2e3f91c57
jin20fd <syj010820@naver.com>
2025-04-24 02:05:01 +0900

    fix: apt로 docker-compose 다운로드하도록 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,12 +14,12 @@ fi
 echo "[1] docker 설치 완료"
 
 if ! command -v docker-compose &> /dev/null; then
-  curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  sudo apt update -y
+  sudo apt install -y docker-compose
 fi
 echo "[2] docker-compose 설치 완료"
 
-echo "[3] 컨테이너 재시작"
+echo "컨테이너 재시작"
 sudo docker-compose down
 DB_ROOT_PASSWORD="${DB_ROOT_PASSWORD}" DB_USER_PASSWORD="${DB_USER_PASSWORD}" sudo docker-compose pull
 DB_ROOT_PASSWORD="${DB_ROOT_PASSWORD}" DB_USER_PASSWORD="${DB_USER_PASSWORD}" sudo docker-compose up -d
