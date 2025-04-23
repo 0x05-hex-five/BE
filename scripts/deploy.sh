567f2317abb518a683c8ce1dd5a6f4cf75f63580
jin20fd <syj010820@naver.com>
2025-04-24 01:58:33 +0900

    fix: sudo 권한 부여

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,22 +6,22 @@ APP_DIR=~/ismedi
 cd "$APP_DIR"
 
 if ! command -v docker &> /dev/null; then
-    apt update -y
-    apt install -y docker.io
-    systemctl start docker
-    systemctl enable docker
+    sudo apt update -y
+    sudo apt install -y docker.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
 fi
 echo "[1] docker 설치 완료"
 
 if ! command -v docker-compose &> /dev/null; then
   curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 fi
 echo "[2] docker-compose 설치 완료"
 
 echo "[3] 컨테이너 재시작"
-docker-compose down
-DB_ROOT_PASSWORD="${DB_ROOT_PASSWORD}" DB_USER_PASSWORD="${DB_USER_PASSWORD}" docker-compose pull
-DB_ROOT_PASSWORD="${DB_ROOT_PASSWORD}" DB_USER_PASSWORD="${DB_USER_PASSWORD}" docker-compose up -d
+sudo docker-compose down
+DB_ROOT_PASSWORD="${DB_ROOT_PASSWORD}" DB_USER_PASSWORD="${DB_USER_PASSWORD}" sudo docker-compose pull
+DB_ROOT_PASSWORD="${DB_ROOT_PASSWORD}" DB_USER_PASSWORD="${DB_USER_PASSWORD}" sudo docker-compose up -d
 
 echo "[✅ 배포 완료]"
