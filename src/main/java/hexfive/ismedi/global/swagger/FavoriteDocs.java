d38b9e77cb254a6ef53bf03d217c19237f21b92d
SeungRyeol Lim <tmdfuf1201@naver.com>
2025-05-04 12:31:43 +0900

    docs: Swagger 문서에서 인증 사용자 정보 숨김 처리

--- src/main/java/hexfive/ismedi/global/swagger/FavoriteDocs.java
+++ src/main/java/hexfive/ismedi/global/swagger/FavoriteDocs.java
@@ -18,11 +18,17 @@ public interface FavoriteDocs {
     @Operation(summary = "즐겨찾기 등록", description = "의약품 ID를 기반으로 해당 의약품을 현재 사용자 즐겨찾기에 추가합니다.")
     APIResponse<String> createFavorites(
             @PathVariable("medicine-id") Long medicineId,
-            @AuthenticationPrincipal UserDetails userDetails);
+            @Parameter(hidden = true) UserDetails userDetails
+    );
 
     @Operation(summary = "즐겨찾기 전체 조회", description = "현재 로그인한 사용자의 즐겨찾기 목록을 조회합니다.")
-    APIResponse<List<ResFavoriteDto>> getAllFavorites(@AuthenticationPrincipal UserDetails userDetails);
+    APIResponse<List<ResFavoriteDto>> getAllFavorites(
+            @Parameter(hidden = true) UserDetails userDetails
+    );
 
     @Operation(summary = "즐겨찾기 삭제", description = "해당 의약품을 즐겨찾기에서 삭제합니다.")
-    APIResponse<String> deleteFavorites(@PathVariable("medicine-id") Long medicineId, @AuthenticationPrincipal UserDetails userDetails);
+    APIResponse<String> deleteFavorites(
+            @PathVariable("medicine-id") Long medicineId,
+            @Parameter(hidden = true) UserDetails userDetails
+    );
 }
