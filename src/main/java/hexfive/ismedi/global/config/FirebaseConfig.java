e2e6b9e7c4147cb11fad902e2e84c3eec0e3dabe
jin20fd <syj010820@naver.com>
2025-05-07 15:43:12 +0900

    refact: firebase json 파일 환경변수로 등록

--- src/main/java/hexfive/ismedi/global/config/FirebaseConfig.java
+++ src/main/java/hexfive/ismedi/global/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
@@ -11,12 +12,14 @@ import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+    @Value("${firebase.key.path}")
+    private String firebaseKeyPath;
 
     @PostConstruct
     public void initialize() {
         try {
             FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/firebaseKey.json");
+                    new FileInputStream(firebaseKeyPath);
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
